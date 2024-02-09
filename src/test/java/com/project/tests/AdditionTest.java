d3064ccbd2363e2a2b09c7182ffe296ddaa12f05
Manikandan <manikandanm441@gmail.com>
2024-02-09 22:54:38 +0530

    hi

--- src/test/java/com/project/tests/AdditionTest.java
+++ src/test/java/com/project/tests/AdditionTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class AdditionTest {
 
@@ -20,6 +21,7 @@ public class AdditionTest {
 		 driver.manage().window().maximize();
 		 driver.get("https://www.saucedemo.com/");
 		 String title = driver.getTitle();
-		 Assert.assertEquals(title,"Swag Labs1");
+		
+		Assert.assertEquals(title,"Swag Labs");
 		 }
 }
